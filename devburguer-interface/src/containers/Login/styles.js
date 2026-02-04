1e55ecbd834824746fc7eba423844cec3961b80a
RAFAEL <rafaelx277@gmail.com>
2026-02-04 11:14:18 -0300

    criando components styles

--- devburguer-interface/src/containers/Login/styles.js
+++ devburguer-interface/src/containers/Login/styles.js
@@ -1,38 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background-color: #f5f5f5;
-`; 
-export const LeftContainer = styled.div`
-  flex: 1;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #fff;
-`;
 
-export const RightContainer = styled.div`
-  flex: 1;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background-color: #e0e0e0;
-`;
-export const Title = styled.h1`
-  font-size: 24px;
-    margin-bottom: 20px;
-`;
-export const InputContainer = styled.div`
-  display: flex;
-    flex-direction: column;
-    margin-bottom: 15px;
-`;
+export const Container = styled.div``; 
+
+
+export const LeftContainer = styled.div``;
+
+
+export const RightContainer = styled.div``;
+
+
+export const Title = styled.h2``;
+
+export const Form = styled.form``;
+
+export const InputContainer = styled.div``;
+
+export const Button = styled.button``;
 
 export const Link = styled.a`
   color: #007bff;
--- devburguer-interface/src/main.jsx
+++ devburguer-interface/src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GlobalStyle } from './styles/globalStyles.js'
 
+
 import { Login } from './containers/Login/index.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
