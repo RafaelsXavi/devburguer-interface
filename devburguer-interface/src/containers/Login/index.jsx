2c778f164cc55bd17b05bcab3a55eb33dae9d65f
RAFAEL <rafaelx277@gmail.com>
2026-02-04 11:21:17 -0300

    correção de importação

--- devburguer-interface/src/containers/Login/index.jsx
+++ devburguer-interface/src/containers/Login/index.jsx
@@ -1,37 +1,30 @@
-import { Container } from "./styles";
+import { Container, LeftContainer, RightContainer, Title, Form, InputContainer, Link } from "./styles";
 import Logo from "../../assets/Logo.svg";
 
 export function Login() {
     return (
         
+
         <Container>
-
             <LeftContainer>
-<img src={Logo} alt="logo-devburguer" />
+                <img src={Logo} alt="logo-devburguer" />
             </LeftContainer>
-
             <RightContainer>
                 <Title>Olá, seja bem vindo ao <span>Dev Burguer !.</span> Acesse com seu <span>Login e senha.</span></Title>
                 <Form>
-
-                    <inputContainer>
-                    <label>Email</label>
-                    <input type="email" placeholder="Email" />
-                    </inputContainer>
-
-                    <inputContainer>
-                    <label>Senha</label>
-                    <input type="password" placeholder="Senha" />
-                    </inputContainer>
-
+                    <InputContainer>
+                        <label>Email</label>
+                        <input type="email" placeholder="Email" />
+                    </InputContainer>
+                    <InputContainer>
+                        <label>Senha</label>
+                        <input type="password" placeholder="Senha" />
+                    </InputContainer>
                     <Link href="#">Esqueci minha senha</Link>
-
                     <button type="submit">Entrar</button>
                 </Form>
-
-                <link href="#">Não possui conta? Clique aqui</link>
+                <Link href="#">Não possui conta? Clique aqui</Link>
             </RightContainer>
-
         </Container>
 
     );
