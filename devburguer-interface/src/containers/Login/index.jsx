ed01aa1661f759b4c74defbb60a5a1d2d2bbc482
RAFAEL <rafaelx277@gmail.com>
2026-02-13 10:27:30 -0300

    tela de login

--- devburguer-interface/src/containers/Login/index.jsx
+++ devburguer-interface/src/containers/Login/index.jsx
@@ -1,21 +1,24 @@
 import { Container, LeftContainer, RightContainer, Title, Form, InputContainer, Link, Button } from "./styles";
 
-import Fundoescuro from "../../assets/backgroundlimpo.svg";
-
-import background from '../../assets/backgroundlimpo.svg';
+import backgroundlogo from "../../assets/background-logo.svg";
 
 export function Login() {
     return (
-        
+
 
         <Container>
+
             <LeftContainer>
-                <img src={Fundoescuro} alt="background-devburguer" />
+                <img src={backgroundlogo} alt="background-devburguer" />
             </LeftContainer>
+
             <RightContainer>
+
                 <Title>Olá, seja bem vindo ao <span>Dev Burguer !.</span>
-                <br/>
-                 Acesse com seu <span>Login e senha.</span></Title>
+                    <br />
+                    Acesse com seu <span>Login e senha.</span>
+                </Title>
+
                 <Form>
                     <InputContainer>
                         <label>Email</label>
@@ -25,11 +28,16 @@ export function Login() {
                         <label>Senha</label>
                         <input type="password" placeholder="Senha" />
                     </InputContainer>
-                    
+
                     <Button type="submit">Entrar</Button>
                 </Form>
-                <Link href="#">Não possui conta? Clique aqui</Link>
+
+             <p>
+               Não possui conta? <Link href="#"> Clique aqui</Link>
+            </p>
             </RightContainer>
+
+
         </Container>
 
     );
--- devburguer-interface/src/containers/Login/styles.js
+++ devburguer-interface/src/containers/Login/styles.js
@@ -1,31 +1,30 @@
 import styled from 'styled-components';
-import BackgroundLogo from '../../assets/background-logo.svg';
 import Background from '../../assets/background.svg';
 import logo from '../../assets/Logo.svg';
 
 export const Container = styled.div`
   display: flex;
-    height: 100vh;
-    width: 100vw;
-`; 
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+`;
 
 
 export const LeftContainer = styled.div`
-background: url('${Logo}');
+background: url('${logo}');
 background-size: cover;
 background-position: center;
 
 
 height: 100%;
-width: 100%;
-max-width: 50%;
+width: 50%;
 
 display: flex;
 align-items: center;
 justify-content: center;
  
 img {
-    width: 80%;
+    width: 100%;
     height: auto;
 }
 `;
@@ -38,28 +37,71 @@ align-items: center;
 flex-direction: column;
 
 height: 100%;
-width: 100%;
-max-width: 50%;
+width: 50%;
 
 background: url('${Background}');
 background-color: #1e1e1e;
+p {
+  color: #fff;
+font-size: 16px;
+font-weight: 500;
+}
 
 `;
 
 
 export const Title = styled.h2`
-font-family: 'road rage', sans-serif;
-font-size: 24px;
+font-family: 'Road Rage', sans-serif;
+font-size: 40px;
 color: #fff;
+
+span {
+    color: #9758a6;
+    font-family: 'Road Rage', sans-serif;
+}
 
 
 `;
 
-export const Form = styled.form``;
+export const Form = styled.form`
+display: flex;
+flex-direction: column;
+gap: 20px;
+padding: 20px;
+width: 100%;
+max-width: 400px;
+align-items: center;
+justify-content: center;
 
-export const InputContainer = styled.div``;
+`;
 
-export const Button = styled.button``;
+export const InputContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap: 5px;
+width: 100%;
+
+input {
+  width: 100%;
+  border: none;
+  height: 52px;
+  border-radius: 8px;
+  padding: 0 15px;
+
+}
+
+label {
+  font-size: 18px;
+  font-weight: 500;
+  color: #fff;
+}
+
+`;
+
+export const Button = styled.button`
+
+
+`;
 
 export const Link = styled.a`
   color: #007bff;
